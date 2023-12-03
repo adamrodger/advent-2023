b48d10aa9af88ca184be1fddffb0bde0b242c8a1
Adam Rodger <adam.rodger@gmail.com>
2023-12-03 10:09:01 +0000

    day(3): Part 2

--- src/AdventOfCode/Day3.cs
+++ src/AdventOfCode/Day3.cs
@@ -61,12 +61,66 @@ namespace AdventOfCode
 
         public int Part2(string[] input)
         {
-            foreach (string line in input)
+            var symbols = new Dictionary<Point2D, Symbol>();
+            var parts = new List<Part>();
+
+            // parse
+            for (int y = 0; y < input.Length; y++)
             {
-                throw new NotImplementedException("Part 2 not implemented");
+                string line = input[y];
+
+                for (int x = 0; x < line.Length; x++)
+                {
+                    char c = line[x];
+
+                    if (c == '.')
+                    {
+                        continue;
+                    }
+
+                    if (!char.IsAsciiDigit(c))
+                    {
+                        Point2D location = new Point2D(x, y);
+                        symbols[location] = new Symbol(c, x, y);
+                        continue;
+                    }
+
+                    ReadOnlySpan<char> number = line.Skip(x).TakeWhile(char.IsAsciiDigit).ToArray();
+                    parts.Add(new Part(int.Parse(number), x, x + number.Length - 1, y));
+                    x += number.Length - 1;
+                }
             }
 
-            return 0;
+            int total = 0;
+
+            foreach ((Point2D location, Symbol symbol) in symbols)
+            {
+                if (symbol.Value != '*')
+                {
+                    continue;
+                }
+
+                List<Part> adjacent = new List<Part>();
+
+                // is it adjacent to 2 numbers?
+                foreach (Part part in parts)
+                {
+                    if (symbol.Column >= part.Start - 1
+                     && symbol.Column <= part.End + 1
+                     && symbol.Row >= part.Row - 1
+                     && symbol.Row <= part.Row + 1)
+                    {
+                        adjacent.Add(part);
+                    }
+                }
+
+                if (adjacent.Count == 2)
+                {
+                    total += adjacent[0].Value * adjacent[1].Value;
+                }
+            }
+
+            return total;
         }
 
         private record Part(int Value, int Start, int End, int Row);
