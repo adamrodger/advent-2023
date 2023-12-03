87eb0c7ba67ba74bb7f7fc3bfdeac936c1f2cfd7
Adam Rodger <adam.rodger@gmail.com>
2023-12-03 10:39:08 +0000

    day(3): Even tidier

--- src/AdventOfCode/Day3.cs
+++ src/AdventOfCode/Day3.cs
@@ -14,46 +14,30 @@ namespace AdventOfCode
         {
             Schematic schematic = Schematic.Parse(input);
 
-            int total = 0;
-
-            foreach (Part part in schematic.Parts)
-            {
-                if (part.Edges().Any(schematic.Symbols.ContainsKey))
-                {
-                    total += part.Value;
-                }
-            }
-
-            return total;
+            return schematic.Parts
+                            .Where(p => schematic.Symbols.Any(s => s.IsAdjacent(p)))
+                            .Select(p => p.Value)
+                            .Sum();
         }
 
         public int Part2(string[] input)
         {
             Schematic schematic = Schematic.Parse(input);
 
-            int total = 0;
-
-            foreach (Symbol symbol in schematic.Symbols.Values.Where(s => s.Value == '*'))
-            {
-                // is it adjacent to 2 parts?
-                // TODO: Improve perf by not allocating an array for each search
-                Part[] adjacent = schematic.Parts.Where(symbol.IsAdjacent).ToArray();
-
-                if (adjacent.Length == 2)
-                {
-                    total += adjacent[0].Value * adjacent[1].Value;
-                }
-            }
-
-            return total;
+            return schematic.Symbols
+                            .Where(s => s.Value == '*')
+                            .Select(s => schematic.Parts.Where(s.IsAdjacent).ToArray())
+                            .Where(adjacent => adjacent.Length == 2)
+                            .Select(a => a[0].Value * a[1].Value)
+                            .Sum();
         }
 
         /// <summary>
         /// The engine schematic, with the parts and symbols that comprise it
         /// </summary>
         /// <param name="Parts">Parts</param>
-        /// <param name="Symbols">Symbols, indexed by their location</param>
-        private readonly record struct Schematic(ICollection<Part> Parts, IDictionary<Point2D, Symbol> Symbols)
+        /// <param name="Symbols">Symbols</param>
+        private readonly record struct Schematic(ICollection<Part> Parts, ICollection<Symbol> Symbols)
         {
             /// <summary>
             /// Parse the schematic from the input
@@ -62,7 +46,7 @@ namespace AdventOfCode
             /// <returns>Parsed engine schematic</returns>
             public static Schematic Parse(IEnumerable<string> input)
             {
-                var symbols = new Dictionary<Point2D, Symbol>();
+                var symbols = new List<Symbol>();
                 var parts = new List<Part>();
 
                 foreach ((int y, string line) in input.Enumerate())
@@ -78,13 +62,15 @@ namespace AdventOfCode
 
                         if (!char.IsAsciiDigit(c))
                         {
-                            Point2D location = new Point2D(x, y);
-                            symbols[location] = new Symbol(c, x, y);
+                            symbols.Add(new Symbol(c, x, y));
                             continue;
                         }
 
+                        // TODO: Improve perf using Spans instead of allocating an array
                         ReadOnlySpan<char> number = line.Skip(x).TakeWhile(char.IsAsciiDigit).ToArray();
                         parts.Add(new Part(int.Parse(number), x, x + number.Length - 1, y));
+
+                        // skip over the bits we parsed
                         x += number.Length - 1;
                     }
                 }
@@ -100,32 +86,7 @@ namespace AdventOfCode
         /// <param name="Start">The start index of the part number</param>
         /// <param name="End">The end index of the part number</param>
         /// <param name="Row">The row which contains the part</param>
-        private record Part(int Value, int Start, int End, int Row)
-        {
-            /// <summary>
-            /// Walk around all the edge locations of the part
-            /// </summary>
-            /// <returns>Edge locations</returns>
-            public IEnumerable<Point2D> Edges()
-            {
-                // left side
-                yield return (this.Start - 1, this.Row - 1);
-                yield return (this.Start - 1, this.Row);
-                yield return (this.Start - 1, this.Row + 1);
-                
-                // right side
-                yield return (this.End + 1, this.Row - 1);
-                yield return (this.End + 1, this.Row);
-                yield return (this.End + 1, this.Row + 1);
-
-                // top and bottom
-                for (int x = this.Start; x <= this.End; x++)
-                {
-                    yield return (x, this.Row - 1);
-                    yield return (x, this.Row + 1);
-                }
-            }
-        }
+        private record Part(int Value, int Start, int End, int Row);
 
         /// <summary>
         /// A symbol on the engine schematic
