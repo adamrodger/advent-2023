5a82124c05dfd5f9a8d522d893119af9079dc815
Adam Rodger <adam.rodger@gmail.com>
2023-12-08 07:29:16 +0000

    day(8): Part 2

--- src/AdventOfCode/Day8.cs
+++ src/AdventOfCode/Day8.cs
@@ -20,11 +20,43 @@ namespace AdventOfCode
                 steps[line[..3]] = (line[7..10], line[12..15]);
             }
 
-            string current = "AAA";
+            return StepLength("AAA", x => x == "ZZZ", instructions, steps);
+        }
+
+        public long Part2(string[] input)
+        {
+            // this is going to be finding the loop length of each one and then LCM of those
+
+            string instructions = input[0];
+
+            Dictionary<string, (string Left, string Right)> steps = new(input.Length - 2);
+
+            foreach (string line in input.Skip(2))
+            {
+                steps[line[..3]] = (line[7..10], line[12..15]);
+            }
+
+            string[] startNodes = steps.Keys.Where(s => s.EndsWith('A')).ToArray();
+            long[] counts = new long[startNodes.Length];
+
+            foreach ((string start, int i) in startNodes.Select((s, i) => (s, i)))
+            {
+                counts[i] = StepLength(start, x => x.EndsWith('Z'), instructions, steps);
+            }
+
+            return counts.Aggregate(findLCM);
+        }
+
+        private static int StepLength(string start,
+                                      Predicate<string> target,
+                                      string instructions,
+                                      IDictionary<string, (string Left, string Right)> steps)
+        {
+            string current = start;
             int i = 0;
             int total = 0;
 
-            while (current != "ZZZ")
+            while (!target(current))
             {
                 char instruction = instructions[i];
 
@@ -39,14 +71,29 @@ namespace AdventOfCode
             return total;
         }
 
-        public int Part2(string[] input)
+        public static long findLCM(long a, long b)
         {
-            foreach (string line in input)
+            long num1, num2;
+
+            if (a > b)
             {
-                throw new NotImplementedException("Part 2 not implemented");
+                num1 = a;
+                num2 = b;
+            }
+            else
+            {
+                num1 = b;
+                num2 = a;
             }
 
-            return 0;
+            for (int i = 1; i <= num2; i++)
+            {
+                if ((num1 * i) % num2 == 0)
+                {
+                    return i * num1;
+                }
+            }
+            return num2;
         }
     }
 }
