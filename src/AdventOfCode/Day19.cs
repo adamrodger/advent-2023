c570c9b57b716a980a26416169056b42850e1ee9
Adam Rodger <adam.rodger@gmail.com>
2023-12-19 08:56:57 +0000

    day(19): Part 2 too high

--- src/AdventOfCode/Day19.cs
+++ src/AdventOfCode/Day19.cs
@@ -57,14 +57,40 @@ namespace AdventOfCode
             return total;
         }
 
-        public int Part2(string[] input)
+        public long Part2(string[] input)
         {
-            foreach (string line in input)
+            Dictionary<string, PartRule> rules = input.TakeWhile(line => !string.IsNullOrEmpty(line))
+                                                      .Select(PartRule.Parse)
+                                                      .ToDictionary(r => r.Id);
+
+            // 171210436954933152 -- too high
+
+            return PossibleCombinations(rules["in"], PartBounds.Default, rules);
+        }
+
+        private long PossibleCombinations(PartRule current, PartBounds bounds, Dictionary<string, PartRule> rules)
+        {
+            long total = 0;
+
+            foreach (PartPredicate predicate in current.Predicates)
             {
-                throw new NotImplementedException("Part 2 not implemented");
+                if (predicate.Destination == "R")
+                {
+                    return 0;
+                }
+
+                if (predicate.Destination == "A")
+                {
+                    return bounds.Possibilities;
+                }
+
+                PartRule destination = rules[predicate.Destination];
+
+                total += PossibleCombinations(destination, bounds.MatchedBounds(predicate), rules);
+                total += PossibleCombinations(destination, bounds.NotMatchedBounds(predicate), rules);
             }
 
-            return 0;
+            return total;
         }
 
         private record Part(int X, int M, int A, int S)
@@ -99,7 +125,7 @@ namespace AdventOfCode
             }
         }
 
-        private record PartPredicate(Predicate<Part> Predicate, string Destination)
+        private record PartPredicate(Predicate<Part> Predicate, char Property, PartOperation Operation, int Value, string Destination)
         {
             public static PartPredicate Parse(string input)
             {
@@ -111,44 +137,94 @@ namespace AdventOfCode
                 int colonIndex = input.IndexOf(':');
 
                 Predicate<Part> predicate;
+                PartOperation operation;
                 string destination;
+                int value;
 
                 if (input.Contains('<'))
                 {
-                    int n = int.Parse(input[2..colonIndex]);
+                    value = int.Parse(input[2..colonIndex]);
 
                     predicate = property switch
                     {
-                        'x' => part => part.X < n,
-                        'm' => part => part.M < n,
-                        'a' => part => part.A < n,
-                        's' => part => part.S < n,
+                        'x' => part => part.X < value,
+                        'm' => part => part.M < value,
+                        'a' => part => part.A < value,
+                        's' => part => part.S < value,
                     };
+
+                    operation = PartOperation.LessThan;
 
                     destination = input[(colonIndex + 1)..];
                 }
                 else if (input.Contains('>'))
                 {
-                    int n = int.Parse(input[2..colonIndex]);
+                    value = int.Parse(input[2..colonIndex]);
 
                     predicate = property switch
                     {
-                        'x' => part => part.X > n,
-                        'm' => part => part.M > n,
-                        'a' => part => part.A > n,
-                        's' => part => part.S > n,
+                        'x' => part => part.X > value,
+                        'm' => part => part.M > value,
+                        'a' => part => part.A > value,
+                        's' => part => part.S > value,
                     };
+
+                    operation = PartOperation.GreaterThan;
 
                     destination = input[(colonIndex + 1)..];
                 }
                 else
                 {
                     predicate = _ => true;
+                    operation = PartOperation.Follow;
                     destination = input;
+                    value = 0;
                 }
 
-                return new PartPredicate(predicate, destination);
+                return new PartPredicate(predicate, property, operation, value, destination);
             }
+        }
+
+        private enum PartOperation
+        {
+            GreaterThan,
+            LessThan,
+            Follow
+        }
+
+        private record PartBounds(int MinX, int MaxX, int MinM, int MaxM, int MinA, int MaxA, int MinS, int MaxS)
+        {
+            public static readonly PartBounds Default = new(1, 4000, 1, 4000, 1, 4000, 1, 4000);
+
+            public long Possibilities => (long)(MaxX - MinX) * (MaxM - MinM) * (MaxA - MinA) * (MaxS - MinS);
+
+            public PartBounds MatchedBounds(PartPredicate predicate) => (predicate.Property, predicate.Operation) switch
+            {
+                (_, PartOperation.Follow) => this,
+                ('x', PartOperation.LessThan) => this with { MaxX = predicate.Value - 1 },
+                ('x', PartOperation.GreaterThan) => this with { MinX = predicate.Value + 1 },
+                ('m', PartOperation.LessThan) => this with { MaxM = predicate.Value - 1 },
+                ('m', PartOperation.GreaterThan) => this with { MinM = predicate.Value + 1 },
+                ('a', PartOperation.LessThan) => this with { MaxA = predicate.Value - 1 },
+                ('a', PartOperation.GreaterThan) => this with { MinA = predicate.Value + 1 },
+                ('s', PartOperation.LessThan) => this with { MaxS = predicate.Value - 1 },
+                ('s', PartOperation.GreaterThan) => this with { MinS = predicate.Value + 1 },
+                _ => throw new ArgumentOutOfRangeException(nameof(predicate), predicate, $"Invalid predicate {predicate}")
+            };
+
+            public PartBounds NotMatchedBounds(PartPredicate predicate) => (predicate.Property, predicate.Operation) switch
+            {
+                (_, PartOperation.Follow) => this,
+                ('x', PartOperation.LessThan) => this with { MinX = predicate.Value },
+                ('x', PartOperation.GreaterThan) => this with { MaxX = predicate.Value },
+                ('m', PartOperation.LessThan) => this with { MinM = predicate.Value },
+                ('m', PartOperation.GreaterThan) => this with { MaxM = predicate.Value },
+                ('a', PartOperation.LessThan) => this with { MinA = predicate.Value },
+                ('a', PartOperation.GreaterThan) => this with { MaxA = predicate.Value },
+                ('s', PartOperation.LessThan) => this with { MinS = predicate.Value },
+                ('s', PartOperation.GreaterThan) => this with { MaxS = predicate.Value },
+                _ => throw new ArgumentOutOfRangeException(nameof(predicate), predicate, $"Invalid predicate {predicate}")
+            };
         }
     }
 }
