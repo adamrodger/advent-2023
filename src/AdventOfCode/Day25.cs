2b335619b8c5078c77ca0306d8d7c6a5fd14aced
Adam Rodger <adam.rodger@gmail.com>
2023-12-26 17:01:28 +0000

    day(25): Count in code instead of by hand

--- src/AdventOfCode/Day25.cs
+++ src/AdventOfCode/Day25.cs
@@ -27,9 +27,36 @@ namespace AdventOfCode
                 }
             }
 
-            // I just solved this by eye today :D
-            // See the graphviz input file for creating a graph, then the 2 separate sides are obvious, and I just counted one in Paint :D
-            return 732 * (nodes.Count - 732);
+            // Manually found the edges to remove by converting the input to dot format and plotting in Graphviz with:
+            //     dot -Ksfdp -Tsvg day25_graphviz.dot -o day25.svg
+            //
+            // The edges were then removed from day26_graphviz_pruned.dot and the SVG regenerated
+            // to confirm it creates two distinct regions
+            nodes["mfc"].Connections.Remove("vph");
+            nodes["vph"].Connections.Remove("mfc");
+            nodes["vmt"].Connections.Remove("sfm");
+            nodes["sfm"].Connections.Remove("vmt");
+            nodes["rmg"].Connections.Remove("fql");
+            nodes["fql"].Connections.Remove("rmg");
+
+            // now we can just count the nodes in one cluster to get the answer
+            Queue<string> queue = new();
+            queue.Enqueue(nodes.Keys.First());
+
+            HashSet<string> visited = new();
+
+            while (queue.Count > 0)
+            {
+                string current = queue.Dequeue();
+                visited.Add(current);
+
+                foreach (string next in nodes[current].Connections.Where(c => !visited.Contains(c)))
+                {
+                    queue.Enqueue(next);
+                }
+            }
+            
+            return visited.Count * (nodes.Count - visited.Count);
         }
 
         private record Node(string Id, ISet<string> Connections)
